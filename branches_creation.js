e4dc12ef4b46711a817be12541c6586e0e9b55da
Antonio Calapez <antonio@hackages.io>
2017-09-12 21:31:23 +0200

    Dummy commit

---
+++ branches_creation.js
@@ -0,0 +1,24 @@
+const {exec} = require('shelljs');
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+const createBranch = () => {
+  exec(`git checkout --orphan ${guid()}`);
+  exec(`touch lala`);
+  exec(`git add lala`);
+  exec(`git commit -a -m "Dummy commit"`);
+  exec(`rm lala`);
+  exec(`git commit -a -m "Dummy commit 2"`);
+}
+
+Array(50)
+  .fill(0)
+  .forEach(createBranch);
