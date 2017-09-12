48f0ff34257895d6e50bd0302bbc9d5d2779246c
Antonio Calapez <antonio@hackages.io>
2017-09-12 21:13:21 +0200

    Creating the level-2 script

---
+++ level_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function check {
+  DEPENDENCY=$(which $1)
+  if [[ -z "${DEPENDENCY// }" ]]; then
+    eval $(echo ZWNobyAiWW91IGRvbid0IGhhdmUgJDEgaW5zdGFsbGVkIDonKCDwn5ilIg== | base64 --decode)
+    eval $(echo ZWNobyAiUGxlYXNlIGluc3RhbGwgaXQgdGhlbiByZXN0YXJ0IHRoaXMgc2NyaXB0Ig== | base64 --decode)
+    eval $(echo ZXhpdA== | base64 --decode)
+  fi
+  eval $(echo ZWNobyAiR29vZCB5b3UndmUgZ290ICQxIGluc3RhbGxlZCBhbHJlYWR5ISI= | base64 --decode)
+}
+
+echo "Alright, during this one we'll make sure you've got the right tools installed for the job"
+
+eval $(echo Y2hlY2sgIm5wbSI= | base64 --decode)
+eval $(echo Y2hlY2sgInlhcm4i | base64 --decode)
+eval $(echo Y2hlY2sgIm5vZGUi | base64 --decode)
+
+echo "You can checkout level-3-fjdmqfjmfqkm to keep going"
