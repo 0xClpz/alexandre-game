c08e4fbdd8e28ec3c655ebfe5d8607aef1927fe3
Antonio Calapez <antonio@hackages.io>
2017-09-13 09:40:42 +0200

    Removing password for the remote server
    Updated the level-3
    
    whatever
    
    Creating the dockerfiles and everything

--- level_3.sh
+++ level_3.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-echo "Welcome to the level 3!" | ./hollywood
-echo "To proceed to level 4 you're gonna need to SSH into a remote server" | ./hollywood
-echo "I'm gonna give you the username and hostname but not the password 🤔 " | ./hollywood
-echo "To get the password you're gonna have to dig into 'history'" | ./hollywood
-echo "If you need a tip, open this script into a text editor, uncomment the last line and run it again." | ./hollywood
-
-echo "username: level-3" | ./hollywood
-echo "hostname: calapez.me" ./hollywood
-
-# eval $(echo ZWNobyAiQnkgaGlzdG9yeSwgSSBtZWFudCAnZ2l0IGxvZycsIGVub3VnaCBzYWlkLiIgfCAuL2hvbGx5d29vZA== | base64 --decode)
+echo "Welcome to level 3!" | ./hollywood
+echo "Don't worry, this is the last one 😅 " | ./hollywood
+echo "We're gonna make sure you've got Docker installed !" | ./hollywood
+echo "To get the password to your account please run 'docker-compose up' then open your browser @http://localhost:5000" | ./hollywood
